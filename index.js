4db6dc3159b9692ed88ce2836b0d287283dd7e42
AEMelenchuk <melenchuk89@mail.ru>
2023-09-26 16:04:57 +0300

    first commit

--- index.js
+++ index.js
@@ -106,7 +106,6 @@ numberButtons.forEach((button) => {
     calculator.updateDisplay();
   });
 });
-
 operationButtons.forEach((button) => {
   button.addEventListener("click", () => {
     calculator.chooseOperation(button.innerText);
